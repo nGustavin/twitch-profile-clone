4d6d53f8afdbaa0001bacf24d60c6f196541668a
hero <gust4f3rn@gmail.com>
2021-03-25 07:16:13 -0300

    header finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import {HiOutlineChatAlt2} from 'react-icons/hi'
 import TwitchIcon from '../../assets/twitch.svg'
 import BitsIcon from '../../assets/bits.png'
 
-import { Container, RightButtons, LeftButtons, Search } from './styles';
+import { Container, RightButtons, LeftButtons, Search, BitsButton } from './styles';
 
 interface HeaderProps {
   children?: ReactNode;
@@ -24,21 +24,27 @@ function Header({ children }: HeaderProps) {
         </div>
       </LeftButtons>
       <Search>
-        <input type="text"/>
-        <button>
-          <FiSearch/>
+        <input type="text" placeholder="Search"/>
+        <button >
+          <FiSearch color={'#606064'} size={24} style={{fontWeight: 'bold'}}/>
         </button>
       </Search>
       <RightButtons>
-        <CgCrown/>
-        <BsInbox/>
-        <HiOutlineChatAlt2/>
         <button>
-          <img src={BitsIcon} alt="buy bits"/>
-          Buy Bits
+          <CgCrown size={22} color={'#efeff1'} />
         </button>
+        <button >
+          <BsInbox size={22} color={'#efeff1'} />
+        </button>
+        <button>
+          <HiOutlineChatAlt2 size={22} color={'#efeff1'} />
+        </button>
+        <BitsButton>
+          <h1>Buy Bits</h1>
+        </BitsButton>
         <div>
           <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/ca73fcbf-9428-4612-80eb-cddb341f315e-profile_image-70x70.png" alt="profile image"/>
+          <div></div>
         </div>
       </RightButtons>
     </Container>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -18,10 +18,12 @@ export const LeftButtons = styled.div`
     height: 100%;
     align-items: center;
     justify-content: flex-start;
-    gap: 20px;
+    gap: 30px;
+    padding: 0px 10px;
 
     > img{
         width: 27px;
+        cursor: pointer;
     }
 
     >h1{
@@ -29,6 +31,10 @@ export const LeftButtons = styled.div`
         font-size: 18px;
         line-height: 22px;
         color: #efeff1;
+        &:hover{
+                cursor: pointer;
+                color: #a26bf4;
+            }
     }
 
     >div{
@@ -37,7 +43,8 @@ export const LeftButtons = styled.div`
         border-radius: 4px;
         background: none;
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        justify-content: flex-start;
         align-items: center;
         overflow: hidden;
 
@@ -45,6 +52,7 @@ export const LeftButtons = styled.div`
             font-size: 25px;
             color: white;
             font-weight: 500;
+            line-height: 14px;
         }
 
         &:hover{
@@ -55,14 +63,124 @@ export const LeftButtons = styled.div`
 `
 export const RightButtons = styled.div`
     display: flex;
-    background: blue;
+    align-items: center;
+    justify-content: flex-end;
     width: 100%;
     height: 100%;
+    gap: 18px;
+    padding: 0px 10px;
+
+    >button{
+        width: 28px;
+        height: 28px;
+        border-radius: 3px;
+        background: none;
+        &:hover{
+            background: #464649;
+            cursor: pointer;
+        }
+    }
+    
+   >div{
+       display: flex;
+       align-items: center;
+       justify-content: center;
+
+    >img{
+        width: 30px;
+        border-radius: 14px;
+
+        ::after{
+            width: 4px;
+            height: 4px;
+            background: green;
+            border-radius: 2px;
+        }
+    }   
+
+    >div{
+        width: 6px;
+        height: 6px;
+        background: #00f593;
+        border-radius: 3px;
+        margin-bottom: -20px;
+        margin-left: -6px;
+    }
+   }
+
+
+
 `
     
 export const Search = styled.div`
     display: flex;
-    background: red;
     width: 100%;
     height: 100%;
+    align-items: center;
+    justify-content: center;
+    gap: 1px;
+
+    >input{
+        background: #464649;
+        width: 22rem;
+        height: 70%;
+        border-radius: 4px 0px 0px 4px;
+        color: #87878a;
+        font-size: 16px;
+        font-weight: 600;
+        padding: 10px;
+        outline: none;
+
+        &:focus{
+            box-shadow: inset 0px 0px 0px 2px #a26bf4;
+            background: #18181B;
+            transition: 0.1s;
+        }
+
+        &:hover{
+            box-shadow: inset 0px 0px 0px 2px #5e5e60;
+            transition: 0.1s;
+
+            &:focus{
+                box-shadow: inset 0px 0px 0px 2px #a26bf4;
+                background: #18181B;
+                transition: 0.1s;
+            }
+        }
+    }
+
+    >button{
+        background: #29292c;
+        /* width: 40%; */
+        height: 70%;
+        width: 2rem;
+        border-radius: 0px 4px 4px 0px;
+
+        &:hover{
+            background: #3a3a3d;
+            cursor: pointer;
+            transition: 0.2s;
+        }
+    }
+`
+
+export const BitsButton = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 28px;
+    background: #3a3a3d;
+    width: 100px;
+    border-radius: 4px;
+
+    >h1{
+        font-size: 15px;
+        font-weight: 600;  
+        color:white;
+    }
+
+    &:hover{
+        background: #464649;
+        cursor: pointer;
+    }
 `
